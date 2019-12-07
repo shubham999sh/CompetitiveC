4b6284fb93404386dc941d14e8e4ea0e1df506a7
Shubham Sharma <shubhamsharmavks@gmail.com>
2019-12-07 20:03:56 +0530

    Create quadratic.c

---
+++ quadratic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    //variable declaration
+    int quad,input,cof1, cof2, constant;
+    float;
+    printf("Enter the value of 'a', 'b', 'c'");
+    scanf("%d",cof1);
+    scanf("%d",cof2);
+    scanf("%d",constant);
+    // Quadratic Equation Representation ax^2 + bx + c = 0
+    if(((cof2^2)-4*cof1*constant)>0)
+    {
+
+        printf("Perfect square");
+    else
+    {
+        printf("prefect square are not possible fo the given values");
+    }
+    
+return 0;
+}
